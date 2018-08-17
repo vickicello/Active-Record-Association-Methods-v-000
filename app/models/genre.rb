e40da61b9a51184848a45efb36f73450c5c05898
Vicki Hamm <vicki.cello@gmail.com>
2018-08-17 23:55:31 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -7,10 +7,10 @@ class Genre < ActiveRecord::Base
   end
 
   def artist_count
-    self.artist.count
+    self.artists.count
   end
 
   def all_artist_names
-    self.artist.all
+    self.artists.all
   end
 end
