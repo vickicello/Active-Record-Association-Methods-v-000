72c861fc30f70a06027b5dbacdeba240024dfd14
Vicki Hamm <vicki.cello@gmail.com>
2018-08-17 23:55:04 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -7,10 +7,10 @@ class Genre < ActiveRecord::Base
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+    self.artist.count
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+    self.artist.all
   end
 end
